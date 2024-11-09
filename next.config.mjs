568d8283ea68be1e73fc19415a8a0ef490abffa4
Keith Ratner <keith.ratner@gmail.com>
2024-11-08 18:53:42 -0600

    GitHub Actions Workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   sassOptions: {
     includePaths: ['./src/styles'],
+  },
+  basePath: '/newsnav', // Replace with your repository name
+  images: {
+    unoptimized: true,
   },
 };
 
